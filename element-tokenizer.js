6bd62d356df749b391fd733aa899f79aa095b20c
dustin pfister <dustin.pfister@gmail.com>
2019-08-25 12:10:19 -0400

    purge false values

--- element-tokenizer.js
+++ element-tokenizer.js
@@ -1,6 +1,11 @@
 var Tokenizer = function(opt){
     
+    var tokens;
+    
     opt = opt || {};
+    
+    // remove false value elements from array such as empty string
+    opt.purgeFalse = opt.purgeFalse === undefined ? true: opt.purgeFalse;
     
     // Root Element: by default use the first article element, 
     // or fall back to body if nothing is given via opt.el
@@ -9,11 +14,23 @@ var Tokenizer = function(opt){
         return art.length === 0 ? document.body: art[0]
     }()) : opt.el;
     
+    // the filter to use to remove unwanted chars
     opt.filter = opt.filter || function(text){
         return text.replace(/[^a-zA-Z ]/g, '');
     };
     
     
-    return opt.filter(opt.el.innerText).split(' ');
+    tokens = opt.filter(opt.el.innerText).split(' ');
+    
+    // purge all words that are empty strings or false
+    // for any reason
+    if(opt.purgeFalse){
+        tokens = tokens.filter(function(word){
+            return word;
+        });
+    }
+    
+    // return words
+    return tokens;
     
 };
